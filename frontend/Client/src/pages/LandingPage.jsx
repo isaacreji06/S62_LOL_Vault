0f9735f633d3947abeb403d5badb9ec6dc425173
Isaac Reji <isaac.reji@kalvium.community>
2025-01-15 15:13:49 +0530

    milestone 9 completed

---
+++ frontend/Client/src/pages/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+function LandingPage() {
+  return (
+    <div>
+      <div className="flex flex-col items-center justify-start h-screen pt-10">
+          <h1 className="text-4xl font-bold text-blue-900">LOL Vault </h1>
+          <h1 className="text-2xl font-bold text-blue-600">Your 1 stop for memes!</h1>
+      </div>
+    </div>
+  )
+}
+
+export default LandingPage
